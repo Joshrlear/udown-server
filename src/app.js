ce31205163980f9534d77406596b23f424ed6e0e
Josh Lear <joshrlear@gmail.com>
2019-08-24 11:36:40 -0700

    changed env

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ const morganOption = (NODE_ENV === 'production')
 
 app.use(morgan(morganOption))
 app.use(cors({
-    origin: 'https://udown.joshrlear.now.sh',
+    origin: 'http://localhost:3000',
     credentials: true
 }))
 app.use(helmet())
