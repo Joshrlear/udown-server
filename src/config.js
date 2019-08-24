3fc2679de52319b2aa5c1ad519da382b59ee289a
Josh Lear <joshrlear@gmail.com>
2019-08-24 11:51:43 -0700

    updated config

--- src/config.js
+++ src/config.js
@@ -1,11 +1,14 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 module.exports = {
-    PORT: process.env.PORT || 8000,
-    NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL: process.env.DB_URL || 'postgresql://josh-lear:password@localhost/udown',
-    GOOGLE_PLACES_API: process.env.GOOGLE_PLACES_API,
-    GOOGLE_PLACES_ENDPOINT: process.env.GOOGLE_PLACES_ENDPOINT,
-    GOOGLE_MAPS_API_KEY: process.env.GOOGLE_MAPS_API_KEY,
-    NEXMO_ENDPOINT: process.env.NEXMO_ENDPOINT,
-    NEXMO_API_KEY: process.env.NEXMO_API_KEY,
-    NEXMO_API_SECRET: process.env.NEXMO_API_SECRET
-  }
+  PORT: process.env.PORT || 8000,
+  NODE_ENV: process.env.NODE_ENV || 'development',
+  DB_URL: process.env.DB_URL || 'postgresql://josh-lear:password@localhost/udown',
+  GOOGLE_PLACES_API: process.env.GOOGLE_PLACES_API,
+  GOOGLE_PLACES_ENDPOINT: process.env.GOOGLE_PLACES_ENDPOINT,
+  GOOGLE_MAPS_API_KEY: process.env.GOOGLE_MAPS_API_KEY,
+  NEXMO_ENDPOINT: process.env.NEXMO_ENDPOINT,
+  NEXMO_API_KEY: process.env.NEXMO_API_KEY,
+  NEXMO_API_SECRET: process.env.NEXMO_API_SECRET
+}
