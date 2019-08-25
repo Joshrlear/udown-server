4023ae18c3b9131d7b08c4f6642962c3c4409fe5
Josh Lear <joshrlear@gmail.com>
2019-08-24 17:22:28 -0700

    update config DB_URL

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ dotenv.config();
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL || 'postgresql://josh-lear:password@localhost/udown',
+  DB_URL: process.env.DATABASE_URL|| 'postgresql://josh-lear:password@localhost/udown',
   GOOGLE_PLACES_API: process.env.GOOGLE_PLACES_API,
   GOOGLE_PLACES_ENDPOINT: process.env.GOOGLE_PLACES_ENDPOINT,
   GOOGLE_MAPS_API_KEY: process.env.GOOGLE_MAPS_API_KEY,
